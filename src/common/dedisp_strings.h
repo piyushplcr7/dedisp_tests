7de6013ac4510fb2d9aa16742255f535f59544e6
Bram Veenboer <bram.veenboer@gmail.com>
2020-08-25 11:02:28 +0200

    Align ":" in time strings

--- src/common/dedisp_strings.h
+++ src/common/dedisp_strings.h
@@ -1,12 +1,12 @@
 #include <string>
 
-static const std::string init_time_str           = "Initialization time: ";
-static const std::string preprocessing_time_str  = "Preprocessing time:  ";
-static const std::string dedispersion_time_str   = "Dedispersion time:   ";
-static const std::string postprocessing_time_str = "Postprocessing time: ";
-static const std::string input_memcpy_time_str   = "Input memcpy time:   ";
-static const std::string output_memcpy_time_str  = "Output memcpy time:  ";
-static const std::string total_time_str          = "Total time:          ";
+static const std::string init_time_str           = "Initialization time : ";
+static const std::string preprocessing_time_str  = "Preprocessing time  : ";
+static const std::string dedispersion_time_str   = "Dedispersion time   : ";
+static const std::string postprocessing_time_str = "Postprocessing time : ";
+static const std::string input_memcpy_time_str   = "Input memcpy time   : ";
+static const std::string output_memcpy_time_str  = "Output memcpy time  : ";
+static const std::string total_time_str          = "Total time          : ";
 
 static const std::string info_str             = ">> Info";
 static const std::string debug_str            = ">> Debug";
