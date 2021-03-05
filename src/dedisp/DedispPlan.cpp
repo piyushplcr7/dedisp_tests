c7e6d31efb1c97a502c973412d8db63745b10790
Bram Veenboer <bram.veenboer@gmail.com>
2021-03-05 15:25:58 +0100

    Let DedispPlan inherit from GPUPlan

--- src/dedisp/DedispPlan.cpp
+++ src/dedisp/DedispPlan.cpp
@@ -1,14 +1,12 @@
 #include <cmath>
 
-#include <cuda_runtime.h>
-
 #include "DedispPlan.hpp"
-#include "transpose/transpose.hpp"
 
 #include "common/dedisp_types.h"
 #include "common/dedisp_error.hpp"
 #include "common/dedisp_strings.h"
 
+#include "transpose/transpose.hpp"
 #include "dedisperse/dedisperse.h"
 #include "unpack/unpack.h"
 
@@ -26,43 +24,15 @@ namespace dedisp
 DedispPlan::DedispPlan(size_type  nchans,
                        float_type dt,
                        float_type f0,
-                       float_type df)
+                       float_type df) :
+    GPUPlan(nchans, dt, f0, df)
 {
-    cu::checkError();
-
-    int device_idx;
-    cudaGetDevice(&device_idx);
-
     // Check for parameter errors
-    if( nchans > DEDISP_MAX_NCHANS ) {
+    if( nchans > DEDISP_MAX_NCHANS) {
         throw_error(DEDISP_NCHANS_EXCEEDS_LIMIT);
     }
 
-    // Force the df parameter to be negative such that
-    //   freq[chan] = f0 + chan * df.
-    df = -std::abs(df);
-
-    m_dm_count      = 0;
-    m_nchans        = nchans;
-    m_gulp_size     = DEDISP_DEFAULT_GULP_SIZE;
-    m_max_delay     = 0;
-    m_dt            = dt;
-    m_f0            = f0;
-    m_df            = df;
-
-    // Generate delay table and copy to device memory
-    // Note: The DM factor is left out and applied during dedispersion
-    h_delay_table.resize(nchans);
-    generate_delay_table(h_delay_table.data(), nchans, dt, f0, df);
-    d_delay_table.resize(nchans * sizeof(dedisp_float));
-    htodstream.memcpyHtoDAsync(d_delay_table, h_delay_table.data(), d_delay_table.size());
-
-    // Initialise the killmask
-    h_killmask.resize(nchans, (dedisp_bool)true);
-    d_killmask.resize(nchans * sizeof(dedisp_bool));
-    set_killmask((dedisp_bool*)0);
-
-    htodstream.synchronize();
+    m_gulp_size = DEDISP_DEFAULT_GULP_SIZE;
 }
 
 // Destructor
@@ -74,127 +44,9 @@ unsigned long div_round_up(unsigned long a, unsigned long b) {
     return (a-1) / b + 1;
 }
 
-void DedispPlan::generate_delay_table(dedisp_float* h_delay_table, dedisp_size nchans,
-                                      dedisp_float dt, dedisp_float f0, dedisp_float df)
-{
-    for( dedisp_size c=0; c<nchans; ++c ) {
-        dedisp_float a = 1.f / (f0+c*df);
-        dedisp_float b = 1.f / f0;
-        // Note: To higher precision, the constant is 4.148741601e3
-        h_delay_table[c] = 4.15e3/dt * (a*a - b*b);
-    }
-}
-
-dedisp_float DedispPlan::get_smearing(dedisp_float dt, dedisp_float pulse_width,
-                                      dedisp_float f0, dedisp_size nchans, dedisp_float df,
-                                      dedisp_float DM, dedisp_float deltaDM)
-{
-    dedisp_float W         = pulse_width;
-    dedisp_float BW        = nchans * abs(df);
-    dedisp_float fc        = f0 - BW/2;
-    dedisp_float inv_fc3   = 1./(fc*fc*fc);
-    dedisp_float t_DM      = 8.3*BW*DM*inv_fc3;
-    dedisp_float t_deltaDM = 8.3/4*BW*nchans*deltaDM*inv_fc3;
-    dedisp_float t_smear   = std::sqrt(dt*dt + W*W + t_DM*t_DM + t_deltaDM*t_deltaDM);
-    return t_smear;
-}
-
-void DedispPlan::generate_dm_list(std::vector<dedisp_float>& dm_table,
-                                  dedisp_float dm_start, dedisp_float dm_end,
-                                  double dt, double ti, double f0, double df,
-                                  dedisp_size nchans, double tol)
-{
-    // Note: This algorithm originates from Lina Levin
-    // Note: Computation done in double precision to match MB's code
-
-    dt *= 1e6;
-    double f    = (f0 + ((nchans/2) - 0.5) * df) * 1e-3;
-    double tol2 = tol*tol;
-    double a    = 8.3 * df / (f*f*f);
-    double a2   = a*a;
-    double b2   = a2 * (double)(nchans*nchans / 16.0);
-    double c    = (dt*dt + ti*ti) * (tol2 - 1.0);
-
-    dm_table.push_back(dm_start);
-    while( dm_table.back() < dm_end ) {
-        double prev     = dm_table.back();
-        double prev2    = prev*prev;
-        double k        = c + tol2*a2*prev2;
-        double dm = ((b2*prev + std::sqrt(-a2*b2*prev2 + (a2+b2)*k)) / (a2+b2));
-        dm_table.push_back(dm);
-    }
-}
-
 // Public interface
-void DedispPlan::set_device(int device_idx) {
-    cu::Device device(device_idx);
-}
-
 void DedispPlan::set_gulp_size(size_type gulp_size) {
     m_gulp_size = gulp_size;
-}
-
-void DedispPlan::set_killmask(const bool_type* killmask)
-{
-    cu::checkError();
-
-    if( 0 != killmask ) {
-        // Copy killmask to plan (both host and device)
-        h_killmask.assign(killmask, killmask + m_nchans);
-        htodstream.memcpyHtoDAsync(d_killmask, h_killmask.data(), d_killmask.size());
-    }
-    else {
-        // Set the killmask to all true
-        std::fill(h_killmask.begin(), h_killmask.end(), (dedisp_bool)true);
-        htodstream.memcpyHtoDAsync(d_killmask, h_killmask.data(), d_killmask.size());
-    }
-}
-
-void DedispPlan::set_dm_list(const float_type* dm_list,
-                             size_type         count)
-{
-    if( !dm_list ) {
-        throw_error(DEDISP_INVALID_POINTER);
-    }
-    cu::checkError();
-
-    m_dm_count = count;
-    h_dm_list.assign(dm_list, dm_list+count);
-
-    // Copy to the device
-    d_dm_list.resize(m_dm_count * sizeof(dedisp_float));
-    htodstream.memcpyHtoDAsync(d_dm_list, h_dm_list.data(), d_dm_list.size());
-    htodstream.synchronize();
-
-    // Calculate the maximum delay and store it in the plan
-    m_max_delay = dedisp_size(h_dm_list[m_dm_count-1] *
-                              h_delay_table[m_nchans-1] + 0.5);
-}
-
-void DedispPlan::generate_dm_list(float_type dm_start,
-                                  float_type dm_end,
-                                  float_type ti,
-                                  float_type tol)
-{
-    cu::checkError();
-
-    // Generate the DM list (on the host)
-    h_dm_list.clear();
-    generate_dm_list(
-        h_dm_list,
-        dm_start, dm_end,
-        m_dt, ti, m_f0, m_df,
-        m_nchans, tol);
-    m_dm_count = h_dm_list.size();
-
-    // Allocate device memory for the DM list
-    d_dm_list.resize(m_dm_count * sizeof(dedisp_float));
-    htodstream.memcpyHtoDAsync(d_dm_list, h_dm_list.data(), d_dm_list.size());
-    htodstream.synchronize();
-
-    // Calculate the maximum delay and store it in the plan
-    m_max_delay = dedisp_size(h_dm_list[m_dm_count-1] *
-                              h_delay_table[m_nchans-1] + 0.5);
 }
 
 void DedispPlan::execute(size_type        nsamps,
@@ -380,7 +232,7 @@ void DedispPlan::execute_guru(size_type        nsamps,
 
     // Measure the total time of the gulp loop
     cu::Event gulpStart, gulpEnd;
-    htodstream.record(gulpStart);
+    htodstream->record(gulpStart);
 #endif
 
     // Gulp loop
@@ -399,14 +251,14 @@ void DedispPlan::execute_guru(size_type        nsamps,
         input_timer->Start();
 #endif
         // Copy the input data from host to device
-        htodstream.memcpyHtoD2DAsync(
+        htodstream->memcpyHtoD2DAsync(
             d_in,                                 // dst
             in_buf_stride_words * BYTES_PER_WORD, // dst stride
             in + gulp_samp_idx*in_stride,         // src
             in_stride,                            // src stride
             nchan_words * BYTES_PER_WORD,         // width bytes
             nsamps_gulp);                         // height
-        htodstream.synchronize();
+        htodstream->synchronize();
 #ifdef DEDISP_BENCHMARK
         cudaDeviceSynchronize();
         input_timer->Pause();
@@ -460,14 +312,14 @@ void DedispPlan::execute_guru(size_type        nsamps,
 #ifdef DEDISP_BENCHMARK
         output_timer->Start();
 #endif
-        dtohstream.memcpyDtoH2DAsync(
+        dtohstream->memcpyDtoH2DAsync(
             out + gulp_samp_byte_idx,  // dst
             out_stride,                // dst stride
             (byte_type *) d_out,       // src
             out_stride_gulp_bytes,     // src stride
             nsamp_bytes_computed_gulp, // width bytes
             dm_count);                 // height
-        dtohstream.synchronize();
+        dtohstream->synchronize();
 #ifdef DEDISP_BENCHMARK
         cudaDeviceSynchronize();
         output_timer->Pause();
@@ -476,7 +328,7 @@ void DedispPlan::execute_guru(size_type        nsamps,
     } // End of gulp loop
 
 #ifdef DEDISP_BENCHMARK
-    dtohstream.record(gulpEnd);
+    dtohstream->record(gulpEnd);
     gulpEnd.synchronize();
     total_timer->Pause();
 
@@ -506,11 +358,6 @@ void DedispPlan::execute_guru(size_type        nsamps,
               << total_time << std::endl;
     perf_file.close();
 #endif
-}
-
-void DedispPlan::sync()
-{
-    cu::checkError(cudaDeviceSynchronize());
 }
 
 } // end namespace dedisp
