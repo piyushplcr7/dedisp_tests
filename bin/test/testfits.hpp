cef3141a1c55b1d76a1e94d6c872f707a3d9a118
ppanchal <piyush.panchal@sam.math.ethz.ch>
2025-02-03 18:30:34 +0100

    Ingest multiple fits files

--- bin/test/testfits.hpp
+++ bin/test/testfits.hpp
@@ -9,10 +9,10 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
+#include <byteswap.h>
 #include <chrono>
 #include <ctime>
 #include <fcntl.h>
@@ -20,7 +20,6 @@
 #include <iostream>
 #include <limits>
 #include <random>
-#include <byteswap.h>
 
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -86,10 +85,11 @@ void swapEndian(float *val) {
 }
 
 void getDataFromRow(FILE *fptr, unsigned char *rawdata, float *data_scl,
-                    float *data_offs, float *data_wts, int subint, 
-                    size_t data_byte_width, size_t scal_offs_width, size_t nchans, size_t initial_offset) {
+                    float *data_offs, float *data_wts, int subint,
+                    size_t data_byte_width, size_t scal_offs_width,
+                    size_t nchans, size_t initial_offset) {
   // Move to dat_wts col
-  fseek(fptr, initial_offset, SEEK_CUR); 
+  fseek(fptr, initial_offset, SEEK_CUR);
 
   // read dat_wts
   fread(data_wts, 4, nchans, fptr);
@@ -132,13 +132,14 @@ void reduceData(float *reduceddata, unsigned char *rawdata, float *data_scl,
   }
 }
 
-void reduceDataTransposed(float *reduceddata, unsigned char *rawdata, float *data_scl,
-                float *data_offs, float *data_wts, int poln, int subint,
-                size_t nsblk, size_t nchans, int npol, int naxis2) {
+void reduceDataTransposed(float *reduceddata, unsigned char *rawdata,
+                          float *data_scl, float *data_offs, float *data_wts,
+                          int poln, int subint, size_t nsblk, size_t nchans,
+                          int npol, int naxis2) {
   for (int spectra = 0; spectra < nsblk; ++spectra) {
     for (int chan = 0; chan < nchans; ++chan) {
       // Hard coded for zero_off = 0
-      //reduceddata[nsblk * nchans * subint + nchans * spectra + chan] =
+      // reduceddata[nsblk * nchans * subint + nchans * spectra + chan] =
       reduceddata[chan * naxis2 * nsblk + subint * nsblk + spectra] =
           ((float)rawdata[nchans * npol * spectra + nchans * poln + chan] *
                data_scl[nchans * poln + chan] +
@@ -293,18 +294,31 @@ template <typename PlanType> int run(int argc, char **argv) {
   /////////////////////////////////////////////////////////////
   /////////// Parsing the command line arguments //////////////
   /////////////////////////////////////////////////////////////
-
   Cmdline *cmd = parseCmdline(argc, argv);
   showOptionValues();
 
-  const char *filename = *cmd->argv;
+  int nfitsfiles = cmd->argc;
+  char **fitsfiles = cmd->argv;
 
-  if (cmd->numdms >0) {
+  printf("No. of infiles = %d\n", nfitsfiles);
+  for (int i = 0; i < nfitsfiles; ++i) {
+    printf("input file %d: %s \n", i, fitsfiles[i]);
+  }
+
+  if (cmd->numdms > 0) {
     if (cmd->dmstep == 0) {
-      std::cerr << "ERROR: Non zero dmstep value required when specifying numdms!" << std::endl;
+      std::cerr
+          << "ERROR: Non zero dmstep value required when specifying numdms!"
+          << std::endl;
       exit(1);
     }
   }
+  ////////////////////////////////////////////////////////////
+  //////////////// WARNING ///////////////////////////////////
+  // Using only the first fits file for extracting the
+  // relevant parameters. It is assumed implicitly that
+  // the parameters are consistent across all fits files.
+  const char *first_filename = fitsfiles[0];
 
   ////////////////////////////////////////////////////////////
   //// Extracting relevant parameters from the fits file /////
@@ -312,9 +326,9 @@ template <typename PlanType> int run(int argc, char **argv) {
 
   fitsfile *ffptr;
   int status = 0;
-  fits_open_file(&ffptr, filename, READONLY, &status);
+  fits_open_file(&ffptr, first_filename, READONLY, &status);
   if (status != 0) {
-    printf("Error in opening fits file!\n");
+    printf("Error in opening first fits file!\n");
     exit(1);
   }
 
@@ -322,7 +336,7 @@ template <typename PlanType> int run(int argc, char **argv) {
   int num_hdus;
   fits_get_num_hdus(ffptr, &num_hdus, &status);
   printf("No. of HDUs = %d\n", num_hdus);
-  
+
   char comment[80];
   // Moving to the relevant hdu
   fits_movnam_hdu(ffptr, BINARY_TBL, "SUBINT", 0, &status);
@@ -354,7 +368,7 @@ template <typename PlanType> int run(int argc, char **argv) {
   size_t data_byte_width = (size_t)nbin * nchans_read * npol * nsblk;
   printf("data byte width = %ld\n", data_byte_width);
   size_t scal_offs_width = (size_t)nchans_read * npol;
-  printf("offs, scal width = %ld\n",scal_offs_width);
+  printf("offs, scal width = %ld\n", scal_offs_width);
 
   int naxis2;
   fits_read_key(ffptr, TINT, "NAXIS2", &naxis2, comment, &status);
@@ -363,17 +377,17 @@ template <typename PlanType> int run(int argc, char **argv) {
   int colnum = -1;
   fits_get_colnum(ffptr, 0, "DAT_FREQ", &colnum, &status);
 
-  double *freqs = (double *) malloc(sizeof(double) * nchans_read);
+  double *freqs = (double *)malloc(sizeof(double) * nchans_read);
   if (status == COL_NOT_FOUND) {
     printf("Warning!:  Can't find the channel freq column!\n");
-    status = 0;     // Reset status
+    status = 0; // Reset status
   } else {
     int anynull = 0;
-    fits_read_col(ffptr, TDOUBLE, colnum, 1L, 1L,
-                              nchans_read, 0, freqs, &anynull, &status);
+    fits_read_col(ffptr, TDOUBLE, colnum, 1L, 1L, nchans_read, 0, freqs,
+                  &anynull, &status);
   }
 
-  printf("lo, hi freq = %.12f, %.12f\n", freqs[0], freqs[nchans_read-1]);
+  printf("lo, hi freq = %.12f, %.12f\n", freqs[0], freqs[nchans_read - 1]);
 
   fits_close_file(ffptr, &status);
 
@@ -381,34 +395,40 @@ template <typename PlanType> int run(int argc, char **argv) {
   //////////////// Initializing more parameters /////////////////////
   ///////////////////////////////////////////////////////////////////
 
-  dedisp_float dm_start = cmd->lodm;    // pc cm^-3
-  dedisp_float dm_end = cmd->hidm;     // pc cm^-3
+  dedisp_float dm_start = cmd->lodm;      // pc cm^-3
+  dedisp_float dm_end = cmd->hidm;        // pc cm^-3
   dedisp_float pulse_width = cmd->pwidth; // ms
   dedisp_float downsamp = cmd->downsamp;
 
   int device_idx = 0;
 
-  dedisp_float sampletime_base = tbin; 
-  printf("sampletime base = %f\n",sampletime_base);
-  
-  dedisp_float Tobs = tbin * nsblk * naxis2;  // Observation duration in seconds
-  printf("Tobs = %f\n",Tobs);
+  dedisp_float sampletime_base = tbin;
+  printf("sampletime base = %f\n", sampletime_base);
 
-  dedisp_float dt = downsamp * sampletime_base; // s 
-  printf("dt = %f\n",dt);
+  // Observation time for the whole timeseries distributed in multiple
+  // files
+  dedisp_float Tobs =
+      tbin * nsblk * naxis2 * nfitsfiles; // Observation duration in seconds
+  printf("Tobs = %f\n", Tobs);
 
-  dedisp_float f0 = freqs[nchans_read-1]; // MHz (highest channel!)
-  printf("f0 = %f\n",f0);
+  dedisp_float dt = downsamp * sampletime_base; // s
+  printf("dt = %f\n", dt);
+
+  dedisp_float f0 = freqs[nchans_read - 1]; // MHz (highest channel!)
+  printf("f0 = %f\n", f0);
 
   dedisp_size nchans = nchans_read;
 
   // (hifreq-lofreq)/(nchans-1)
-  dedisp_float df = -1.0 * ((double)freqs[nchans_read-1] - (double)freqs[0]) / (nchans - 1); // MHz   (This must be negative!)
-  printf("df = %.15f\n",df);
+  dedisp_float df = -1.0 * ((double)freqs[nchans_read - 1] - (double)freqs[0]) /
+                    (nchans - 1); // MHz   (This must be negative!)
+  printf("df = %.15f\n", df);
 
-  dedisp_float bw = ((double)freqs[nchans_read-1] - (double)freqs[0]) / (nchans - 1) * nchans;  // MHz
-  printf("bw = %f\n",bw);
+  dedisp_float bw = ((double)freqs[nchans_read - 1] - (double)freqs[0]) /
+                    (nchans - 1) * nchans; // MHz
+  printf("bw = %f\n", bw);
 
+  // nsamps scales with nfitsfiles because Tobs is scaled already
   dedisp_size nsamps = Tobs / dt;
   dedisp_float dm_tol = 1.25;
   dedisp_size in_nbits = 8;
@@ -428,10 +448,11 @@ template <typename PlanType> int run(int argc, char **argv) {
   /////////////////////////////////////////////////////////////
   //////////////// Finding byte size of HDUs //////////////////
   /////////////////////////////////////////////////////////////
+  //////////// Assuming consistency in all fits files /////////
 
   FILE *fptr;
 
-  if ((fptr = fopen(filename, "rb")) == NULL) {
+  if ((fptr = fopen(first_filename, "rb")) == NULL) {
     printf("Error! opening file");
     exit(1);
   }
@@ -470,61 +491,79 @@ template <typename PlanType> int run(int argc, char **argv) {
   ////////////////////////////////////////////////////////////
 
   // Longest chunksize 2147479552 for direct read (linux read documentation)
-  long chunksize = 2147479552;
-
-  // Opening file DIRECT and no DIRECT
-  int fd = open(filename, O_RDONLY | O_DIRECT);
-  int fd_nodirect = open(filename, O_RDONLY);
-
-  if (fd == -1) {
-    perror("open");
-    return -1;
-  }
+  //long chunksize = 2147479552; 
+  long chunksize = 1073741824;
 
   // Creating buffer to hold all file contents
   unsigned char *table_full;
   if (posix_memalign((void **)&table_full, 4096, file_size_aligned) != 0) {
     perror("posix_memalign");
-    close(fd);
     return -1;
   }
 
-  // Reading the whole file and timing the read
   auto start_time = std::chrono::high_resolution_clock::now();
-  long chunks =
-      getDataFromRows(fd, table_full, chunksize, file_size, fd_nodirect);
   auto end_time = std::chrono::high_resolution_clock::now();
   auto duration_us = std::chrono::duration_cast<std::chrono::microseconds>(
                          end_time - start_time)
                          .count();
 
-  long megabytes_read = (double)(file_size) / 1e6;
-  std::cout << "read " << chunks << " chunks with chunksize = " << chunksize
-            << ", time: " << (double)duration_us / 1e6 << " seconds"
-            << ", Read speed (MB/s): "
-            << megabytes_read / (double)duration_us * 1e6 << std::endl;
-
-  close(fd);
-  close(fd_nodirect);
-
-  float *rawdata;
-  rawdata = (float *)calloc((size_t)nsblk * naxis2 * nchans, sizeof(float));
   int poln = 0;
 
-  start_time = std::chrono::high_resolution_clock::now();
-  reduceBinaryTable(table_full, rawdata, poln, naxis1, naxis2, nsblk, nchans,
-                    npol, offset_for_data, initial_offset, scal_offs_width);
+  float *rawdata;
+  // Allocating enough memory to hold the reduced data from all files
+  rawdata = (float *)calloc((size_t)nsblk * naxis2 * nchans * nfitsfiles,
+                            sizeof(float));
 
-  end_time = std::chrono::high_resolution_clock::now();
-  duration_us = std::chrono::duration_cast<std::chrono::microseconds>(
-                    end_time - start_time)
-                    .count();
+  // Looping over the files
+  for (int file_idx = 0; file_idx < nfitsfiles; ++file_idx) {
+    const char *filename = fitsfiles[file_idx];
+    // Opening file DIRECT and no DIRECT
+    int fd = open(filename, O_RDONLY | O_DIRECT);
+    int fd_nodirect = open(filename, O_RDONLY);
 
-  std::cout << "reduction finished, time: " << (double)duration_us / 1e6
-            << std::endl;
+    if (fd == -1) {
+      perror("open");
+      return -1;
+    }
+
+    // Reading the whole file and timing the read
+    start_time = std::chrono::high_resolution_clock::now();
+    long chunks =
+        getDataFromRows(fd, table_full, chunksize, file_size, fd_nodirect);
+    end_time = std::chrono::high_resolution_clock::now();
+    duration_us = std::chrono::duration_cast<std::chrono::microseconds>(
+                      end_time - start_time)
+                      .count();
+
+    long megabytes_read = (double)(file_size) / 1e6;
+    std::cout << "read " << chunks << " chunks with chunksize = " << chunksize
+              << " from file " << fitsfiles[file_idx]
+              << ", time: " << (double)duration_us / 1e6 << " seconds"
+              << ", Read speed (MB/s): "
+              << megabytes_read / (double)duration_us * 1e6 << std::endl;
+
+    float *current_rawdata_ptr =
+        rawdata + (size_t)nsblk * naxis2 * nchans * file_idx;
+
+    start_time = std::chrono::high_resolution_clock::now();
+    reduceBinaryTable(table_full, current_rawdata_ptr, poln, naxis1, naxis2,
+                      nsblk, nchans, npol, offset_for_data, initial_offset,
+                      scal_offs_width);
+
+    end_time = std::chrono::high_resolution_clock::now();
+    duration_us = std::chrono::duration_cast<std::chrono::microseconds>(
+                      end_time - start_time)
+                      .count();
+
+    std::cout << "reduction finished, time: " << (double)duration_us / 1e6
+              << std::endl;
+
+    close(fd);
+    close(fd_nodirect);
+  }
 
   free(table_full);
-  
+
   /*
      input is a pointer to an array containing a time series of length
      nsamps for each frequency channel in plan. The data must be in
@@ -534,7 +573,7 @@ template <typename PlanType> int run(int argc, char **argv) {
    */
 
   // input = (dedisp_byte *)malloc(nsamps * nchans * (in_nbits / 8));
-  input = (dedisp_byte*) rawdata;
+  input = (dedisp_byte *)rawdata;
 
   printf("Create plan and init GPU\n");
   // Create a dedispersion plan
@@ -543,11 +582,13 @@ template <typename PlanType> int run(int argc, char **argv) {
   printf("Gen DM list\n");
   // Generate a list of dispersion measures for the plan
   if (cmd->numdms == 0) {
-    std::cout << "Numdms not specified, generating DM list using the internal function" << std::endl;
+    std::cout << "Numdms not specified, generating DM list using the internal "
+                 "function"
+              << std::endl;
     plan.generate_dm_list(dm_start, dm_end, pulse_width, dm_tol);
-  }
-  else {
-    std::cout << "Generating equispaced DM list using the provided step size" << std::endl;
+  } else {
+    std::cout << "Generating equispaced DM list using the provided step size"
+              << std::endl;
     plan.generate_dm_list_equispaced(cmd->lodm, cmd->dmstep, cmd->numdms);
   }
 
@@ -574,11 +615,13 @@ template <typename PlanType> int run(int argc, char **argv) {
          (dm_count * nsamps_computed * (out_nbits / 8)) / (1 << 20));
 #else
   unsigned int nsamp_fft = dedisp::round_up(nsamps + max_delay, 16384);
-  printf("Computing %lu Fourier Coefficients of dedispersed timeseries (adjusting for max delay)\n", nsamp_fft);
+  printf("Computing %lu Fourier Coefficients of dedispersed timeseries "
+         "(adjusting for max delay)\n",
+         nsamp_fft);
   printf("Output data array size : %lu MB\n",
          (dm_count * nsamp_fft * (out_nbits / 8)) / (1 << 20));
 #endif
-  
+
   printf("\n");
 
   // Allocate space for the output data
@@ -593,7 +636,7 @@ template <typename PlanType> int run(int argc, char **argv) {
   }
 
   printf("Compute on GPU\n");
-  //startclock = clock();
+  // startclock = clock();
   aa_gpu_timer timer;
   timer.Start();
   // Compute the dedispersion transform on the GPU
@@ -614,25 +657,25 @@ template <typename PlanType> int run(int argc, char **argv) {
   FILE *file_out = fopen("output.bin", "wb");
 #else
   FILE *file_out = fopen("output.fft", "wb");
-#endif  
+#endif
   start_time = std::chrono::high_resolution_clock::now();
   fwrite(output, 1, (size_t)nsamps_computed * dm_count * out_nbits / 8,
          file_out);
   fclose(file_out);
   end_time = std::chrono::high_resolution_clock::now();
   duration_us = std::chrono::duration_cast<std::chrono::microseconds>(
-                         end_time - start_time)
-                         .count();
+                    end_time - start_time)
+                    .count();
   std::cout << "Writing the output data took " << (double)duration_us / 1e6
             << " seconds" << std::endl;
   // #endif
 
   // Clean up
   free(output);
-  //free(input);
-  #ifdef READFROMFILE
+// free(input);
+#ifdef READFROMFILE
   free(rawdata);
-  #endif
+#endif
   printf("Dedispersion successful.\n");
   return 0;
 }
